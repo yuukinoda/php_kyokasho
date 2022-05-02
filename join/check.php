4c3dd4912bd583f4ddd78a45b8d7edd58e45f960
yuukinoda <ynoda@eiffelcard.com>
2022-05-02 22:30:42 +0900

    chapter6-3

---
+++ join/check.php
@@ -0,0 +1,16 @@
+<form action="" method="post" enctype="multipart/form-data">
+    <dl>
+        <dt>ニックネーム</dt>
+        <dd></dd>
+        <dt>メールアドレス</dt>
+        <dd></dd>
+        <dt>パスワード<span class="required">必須</dt>
+        <dd>【開示されません】</dd>
+        <dt>写真など</dt>
+        <dd></dd>
+    </dl>
+    <div>
+        <a href="index.php?action=rewrite">&laquo;&nbsp;書き直す</a>|
+        <input type="submit" value="登録する"/>
+    </div>
+</form>
---
+++ join/index.php
@@ -0,0 +1,15 @@
+<p>次のフォームに必要事項をご記入ください。</p>
+
+<form action="" method="post" enctype="multipart/form-data">
+    <dl>
+        <dt>ニックネーム<span class="required">必須</span></dt>
+        <dd><input type="text" name="name" size="35" maxlength="255"/></dd>
+        <dt>メールアドレス<span class="required">必須</span></dt>
+        <dd><input type="text" name="email" size="35" maxlength="255"/></dd>
+        <dt>パスワード<span class="required">必須</dt>
+        <dd><input type="password" name="password" size="10" maxlength="20"/></dd>
+        <dt>写真など</dt>
+        <dd><input type="file" name="image" size="35"/></dd>
+        <div><input type="submit" value="入力内容を確認する"/></div>
+    </dl>
+</form>
